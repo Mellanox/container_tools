e946371dc911b5b52cca4dee5d5262e2211e7605
Parav Pandit <parav@mellanox.com>
2019-05-07 21:05:27 -0500

    docker_rdma_sriov: Support modern representor phys_port_id name
    Modern kernel has different phys_port_id consist of PF and VF indices
    such as pfAvfN.
    
    Hence use recent sriovnet library which support mapping uplink netdev
    and VF netdev.
    
    Continue to support older kernel which might have ethX_VfN format.
    
    Signed-off-by: Parav Pandit <parav@mellanox.com>

--- src/docker_rdma_sriov/representor.go
+++ src/docker_rdma_sriov/representor.go
@@ -2,16 +2,35 @@ package main
 
 import (
 	"fmt"
+	"github.com/Mellanox/sriovnet"
 	"github.com/vishvananda/netlink"
 	"strconv"
 )
 
+func getVfRepresentorLink(pfNetdevName string, vfIndex int) (netlink.Link, error) {
+
+	/* First try to get old way eth0_vfIndex */
+	vfRepNetdevName := pfNetdevName + "_" + strconv.Itoa(vfIndex)
+	handle, err := netlink.LinkByName(vfRepNetdevName)
+	if err == nil {
+		fmt.Println("Vf rep:", vfRepNetdevName)
+		return handle, nil
+	}
+
+	/* Possibly new OS with eth0_pfAvfN way scheme */
+	vfRepNetdevName, err = sriovnet.GetVfRepresentor(pfNetdevName, vfIndex)
+	if err != nil {
+		fmt.Println("Representor not found netdev for pf = %v vf %d\n",
+			pfNetdevName, vfIndex)
+		return nil, err
+	}
+	fmt.Println("Vf rep:", vfRepNetdevName)
+	return netlink.LinkByName(vfRepNetdevName)
+}
+
 func SetVfRepresentorLinkUp(pfNetdevName string, vfIndex int) error {
 
-	vfRepNetdevName := pfNetdevName + "_" + strconv.Itoa(vfIndex)
-	fmt.Println("Vf rep:", vfRepNetdevName)
-
-	handle, err := netlink.LinkByName(vfRepNetdevName)
+	handle, err := getVfRepresentorLink(pfNetdevName, vfIndex)
 	if err != nil {
 		return err
 	}
